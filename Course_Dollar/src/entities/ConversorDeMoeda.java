362c234c548b4acc92e5d1bbdb15eb164399817d
leonardolaranjeiradev <leonardolaranjeiradev@gmail.com>
2024-03-06 14:16:59 -0300

    Criado o metodo dollarParaReal

--- Course_Dollar/src/entities/ConversorDeMoeda.java
+++ Course_Dollar/src/entities/ConversorDeMoeda.java
@@ -4,5 +4,8 @@ public class ConversorDeMoeda {
 	
 	public static double IOF = 0.06;
 	
+	public static double dollarParaReal(double quantia, double precoDollar) {
+		return quantia * precoDollar * (1.0 + IOF);
+		}
 	
 }
